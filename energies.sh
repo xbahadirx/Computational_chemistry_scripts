8ef01dad4e783975ded83a70930113740d3666c9
xbahadirx <95621170+xbahadirx@users.noreply.github.com>
2022-02-06 16:26:06 +0300

    Update energies.sh

--- energies.sh
+++ energies.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#by Bahadır ALTINTAŞ
+#Bash script to extract optimized energies from QuantumEspresso geometry optimization output file with given PREFIX and series of Pressure eg "ZrO2_189"
+
 PREFIX="ZrO2_189"
 rm coords.$PREFIX/energies.$PREFIX
 for p in 0 25 50 75 100 125 150 175 200;
