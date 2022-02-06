41e56f71fd6523e8ffc75ddaabf1beebd4a12d87
xbahadirx <95621170+xbahadirx@users.noreply.github.com>
2022-02-06 16:22:31 +0300

    Create coordinates.sh

---
+++ coordinates.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#by Bahadır ALTINTAŞ
+#Bash script to extract optimized coordinates form QuantumEspresso geometry optimization output file with given PREFIX eg "ZrO2_189"
+PREFIX="ZrO2_189"
+for p in 0 ;
+do
+rm $PREFIX.GPa$p.coord
+grep -A15 CELL results/$PREFIX.relax.GPa$p.out | tail -16  >> tmp.coord
+sed '/End final coordinates/d' tmp.coord >> $PREFIX.GPa$p.coord
+rm tmp.coord
+done
